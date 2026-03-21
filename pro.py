5b7540a9e752d299b6999e86e6fa43b19788180b
Gill-Royals <161048737+Gill-Royals@users.noreply.github.com>
2026-03-21 15:51:26 +0530

    Started Putting in classes

--- pro.py
+++ pro.py
@@ -4,29 +4,65 @@ import joblib
 import numpy as np
 from collections import deque
 
+class Mediapipe:
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=1,
+            min_detection_confidence=0.7
+        )
+        self.mp_draw = mp.solutions.drawing_utils
 # Initialize MediaPipe
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    static_image_mode=False,
-    max_num_hands=1,
-    min_detection_confidence=0.7
-)
-
-mp_draw = mp.solutions.drawing_utils
+#mp_hands = mp.solutions.hands
+#hands = mp_hands.Hands(
+#    static_image_mode=False,
+#    max_num_hands=1,
+#    min_detection_confidence=0.7
+#)
+#mp_draw = mp.solutions.drawing_utils
 
 # Load trained model
-model = joblib.load('Models/gesture_model.pkl')
+class ModelLoader:
+    def __init__(self):
+        self.model = joblib.load('Models/gesture_model.pkl')
+#model = joblib.load('Models/gesture_model.pkl')
 
 # Webcam
-cap = cv2.VideoCapture(0)
+class Webcam:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+    
+    def frame(self):
+        success,frame= self.cap.read()
+        if not success:
+            print("Error opening webcam!")
+            exit()
+        else:
+            return frame
+    
+    def release(self):
+        self.cap.release()
+        
+#cap = cv2.VideoCapture(0)
 
 #  Prediction smoothing (reduces flicker)
-history = deque(maxlen=5)
+class PredictionSmoother:
+    def __init__(self):
+        self.history = deque(maxlen=5)
 
+    def smooth(self, prediction):
+        self.history.append(prediction)
+        final_prediction = max(set(self.history), key=self.history.count)
+        return final_prediction
+#history = deque(maxlen=5)
+smooth = PredictionSmoother()
+
+        
 while True:
-    success, frame = cap.read()
+    success, frame = cap.read() 
     if not success:
-        break
+        break 
 
     frame = cv2.flip(frame, 1)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -68,8 +104,9 @@ while True:
             confidence = np.max(prob) * 100
 
             #  Smooth prediction
-            history.append(prediction)
-            final_prediction = max(set(history), key=history.count)
+            #history.append(prediction)
+            #final_prediction = max(set(history), key=history.count)
+            final_prediction= smooth.smooth(prediction)
 
             print(f"{final_prediction} - {confidence:.2f}%")
 
